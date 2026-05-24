db1df7c2cc6b7291b3358bcd33fbee12aaa90267
Konstantin S. <havendv@gmail.com>
2026-05-24 08:12:16 +0400

    feat: Updated OpenAPI spec (#11)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Gamma/Generated/Gamma.GammaClient.GetGenerationStatus.g.cs
+++ src/libs/Gamma/Generated/Gamma.GammaClient.GetGenerationStatus.g.cs
@@ -347,17 +347,16 @@ namespace Gamma
                                     __exception_401 = __ex;
                                 }
 
-                                throw new global::Gamma.ApiException(
+
+                                throw global::Gamma.ApiException.Create(
+                                    statusCode: __response.StatusCode,
                                     message: __content_401 ?? __response.ReasonPhrase ?? string.Empty,
                                     innerException: __exception_401,
-                                    statusCode: __response.StatusCode)
-                                {
-                                    ResponseBody = __content_401,
-                                    ResponseHeaders = global::System.Linq.Enumerable.ToDictionary(
+                                    responseBody: __content_401,
+                                    responseHeaders: global::System.Linq.Enumerable.ToDictionary(
                                         __response.Headers,
                                         h => h.Key,
-                                        h => h.Value),
-                                };
+                                        h => h.Value));
                             }
                             // 
                             if ((int)__response.StatusCode == 404)
@@ -380,17 +379,16 @@ namespace Gamma
                                     __exception_404 = __ex;
                                 }
 
-                                throw new global::Gamma.ApiException(
+
+                                throw global::Gamma.ApiException.Create(
+                                    statusCode: __response.StatusCode,
                                     message: __content_404 ?? __response.ReasonPhrase ?? string.Empty,
                                     innerException: __exception_404,
-                                    statusCode: __response.StatusCode)
-                                {
-                                    ResponseBody = __content_404,
-                                    ResponseHeaders = global::System.Linq.Enumerable.ToDictionary(
+                                    responseBody: __content_404,
+                                    responseHeaders: global::System.Linq.Enumerable.ToDictionary(
                                         __response.Headers,
                                         h => h.Key,
-                                        h => h.Value),
-                                };
+                                        h => h.Value));
                             }
 
                             if (__effectiveReadResponseAsString)
@@ -424,17 +422,15 @@ namespace Gamma
                                 }
                                 catch (global::System.Exception __ex)
                                 {
-                                    throw new global::Gamma.ApiException(
+                                    throw global::Gamma.ApiException.Create(
+                                        statusCode: __response.StatusCode,
                                         message: __content ?? __response.ReasonPhrase ?? string.Empty,
                                         innerException: __ex,
-                                        statusCode: __response.StatusCode)
-                                    {
-                                        ResponseBody = __content,
-                                        ResponseHeaders = global::System.Linq.Enumerable.ToDictionary(
+                                        responseBody: __content,
+                                        responseHeaders: global::System.Linq.Enumerable.ToDictionary(
                                             __response.Headers,
                                             h => h.Key,
-                                            h => h.Value),
-                                    };
+                                            h => h.Value));
                                 }
                             }
                             else
@@ -471,17 +467,15 @@ namespace Gamma
                                     {
                                     }
 
-                                    throw new global::Gamma.ApiException(
+                                    throw global::Gamma.ApiException.Create(
+                                        statusCode: __response.StatusCode,
                                         message: __content ?? __response.ReasonPhrase ?? string.Empty,
                                         innerException: __ex,
-                                        statusCode: __response.StatusCode)
-                                    {
-                                        ResponseBody = __content,
-                                        ResponseHeaders = global::System.Linq.Enumerable.ToDictionary(
+                                        responseBody: __content,
+                                        responseHeaders: global::System.Linq.Enumerable.ToDictionary(
                                             __response.Headers,
                                             h => h.Key,
-                                            h => h.Value),
-                                    };
+                                            h => h.Value));
                                 }
                             }
 
